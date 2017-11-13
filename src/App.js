cfb5a53ebea761e62e9cbf441af93fc018b0ab6e
mugworks <greenwood.pdx@gmail.com>
2017-11-13 10:08:27 -0800

    mockup of page and add state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      itemInHand: null,
+      timer: 0,
+      floors,
+      floor: floors[4],
+      pizzaItems: null
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Pizza</h1>
+          <p>This is your home floor.</p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <select name="elevator">
+            <option value="floor1">Floor 1</option>
+            <option value="floor2">Floor 2</option>
+            <option value="floor3">Floor 3</option>
+            <option value="floor4">Floor 4</option>
+            <option value="floor5">Floor 5</option>
+            <option value="floor6">Floor 6</option>
+            <option value="floor7">Floor 7</option>
+          </select>
+          <button name="pick-up">Pick Up</button>
+          <button name="drop">Drop</button>
+          <button name="pizza-add">Add to Pizza</button>
+          <select name="items-in-room">Options per room</select>
+
+
+        </div>
       </div>
     );
   }
