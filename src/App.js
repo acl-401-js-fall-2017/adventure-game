9798d5a22c56992116692e0fe4e125597ef4c17c
mugworks <greenwood.pdx@gmail.com>
2017-11-13 13:21:18 -0800

    add drop and add logic

--- src/App.js
+++ src/App.js
@@ -18,28 +18,39 @@ class App extends Component {
 
   handlePickUpValue = value => {
     console.log('current pick up value', value);
-    this.setState({ pickUpValue: value })
+    this.setState({ pickUpValue: value });
   };
 
   handlePickUp = value => {
-    console.log('current button value ', value);
-    this.setState({ itemInHand: value })
-    console.log('item in hand ', this.state.itemInHand)
+    if (this.state.itemInHand) return alert ('You can only hold one item');
+    this.setState({ itemInHand: value });
+  }
+
+  handleDrop = () => {
+    this.setState({ itemInHand: null });
   }
 
   handleFloorChange = value => {
-    console.log('new floor ', value);
-    this.setState({ 
-      pickUpValue: floors[value].items[0],
-      floor: floors[value]
-    });
+    if (value == 1) {
+      console.log('are we in floor change', value);
+      this.setState({
+        pizzaItems: [],
+        pickUpValue: floors[value].items[0],
+        floor: floors[value]
+      });
+    } else { 
+      this.setState({ 
+        pickUpValue: floors[value].items[0],
+        floor: floors[value]
+      });
+    }
   }
 
   handleAddtoPizza = value => {
     let pizzaItems = this.state.pizzaItems;
     pizzaItems.push(value);
     console.log('I am the pizza ', pizzaItems);
-    this.setState({ pizzaItems })
+    this.setState({ pizzaItems, itemInHand: null });
   }
 
 
@@ -53,25 +64,24 @@ class App extends Component {
         </header>
         <div>
           <select name="elevator" onChange={({ target }) => this.handleFloorChange(target.value)}>
-            <option value="1">Floor 1</option>
-            <option value="2">Floor 2</option>
-            <option value="3">Floor 3</option>
-            <option value="4">Floor 4</option>
-            <option value="5">Floor 5</option>
-            <option value="6">Floor 6</option>
-            <option value="7">Floor 7</option>
+            <option value="0">Floor 1</option>
+            <option value="1">Floor 2</option>
+            <option value="2">Floor 3</option>
+            <option value="3">Floor 4</option>
+            <option value="4">Floor 5</option>
+            <option value="5">Floor 6</option>
+            <option value="6">Floor 7</option>
           </select>
           { this.state.floor.key === 'floor4' && makePizzaButton }
-          <button name="pick-up" value={ this.state.pickUpValue } onClick={({ target }) => this.handlePickUp(target.value)}>Pick Up</button>
-          <button name="drop">Drop</button>
-          <button name="pizza-add" value={ this.state.itemInHand } onClick={({ target }) => {this.handleAddtoPizza(target.value)}}>Add to Pizza</button>
+          <button name="pick-up" value={this.state.pickUpValue} onClick={({ target }) => this.handlePickUp(target.value)}>Pick Up</button>
+          <button name="drop" onClick= {() => this.handleDrop()}>Drop</button>
+          <button name="pizza-add" value={this.state.itemInHand} onClick={({ target }) => {this.handleAddtoPizza(target.value)}}>Add to Pizza</button>
           <label>Items in the Room</label>
           <select name="items-in-room" onChange={({ target }) => {
-            console.log('item in hand 232323232323', this.state.itemInHand)
-            this.handlePickUpValue(target.value)}}>
-          { this.state.floor.items.map((item) => {
-            return <option value={item}>{item}</option>;
-          }) }
+            this.handlePickUpValue(target.value);}}>
+            { this.state.floor.items.map((item) => {
+              return <option value={item}>{item}</option>;
+            }) }
           </select>
           
 
