80acb640563f9d2d2e85d323930579bf1144d9e5
mirrorapps <eli.speigel@gmail.com>
2017-11-12 21:43:41 -0800

    setup additional imports and constructor/state for component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Stage from './Stage';
+import Player from './Player';
+import stages from './stages';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      stages,
+      stage: stages[0],
+      player: {
+        name: 'Big Shaq',
+        pockets: []
+      },
+
+    };
+  }
   render() {
     return (
       <div className="App">
