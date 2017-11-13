0d91427584b73b92a99871d2a167665631d7e50d
Shane Moyo <smoyo@pdx.edu>
2017-11-13 12:40:39 -0800

    added a few buttons

--- src/App.js
+++ src/App.js
@@ -11,16 +11,40 @@ class App extends Component {
       timer: 0,
       floors,
       floor: floors[1],
-      // pickUpValue: floor.items[0],
-      pizzaItems: null
+      pickUpValue: floors[1].items[0],
+      pizzaItems: []
     };
   }
 
-  // handlePickUp = item => {
-  //   onClick = 
-  // };
+  handlePickUpValue = value => {
+    console.log('current pick up value', value);
+    this.setState({ pickUpValue: value })
+  };
+
+  handlePickUp = value => {
+    console.log('current button value ', value);
+    this.setState({ itemInHand: value })
+    console.log('item in hand ', this.state.itemInHand)
+  }
+
+  handleFloorChange = value => {
+    console.log('new floor ', value);
+    this.setState({ 
+      pickUpValue: floors[value].items[0],
+      floor: floors[value]
+    });
+  }
+
+  handleAddtoPizza = value => {
+    let pizzaItems = this.state.pizzaItems;
+    pizzaItems.push(value);
+    console.log('I am the pizza ', pizzaItems);
+    this.setState({ pizzaItems })
+  }
+
 
   render() {
+    const makePizzaButton = <button name="make-pizza">make pizza</button>;
     return (
       <div className="App">
         <header className="App-header">
@@ -28,22 +52,27 @@ class App extends Component {
           <p>{ this.state.floor.message }</p>
         </header>
         <div>
-          <select name="elevator">
-            <option value="floor1">Floor 1</option>
-            <option value="floor2">Floor 2</option>
-            <option value="floor3">Floor 3</option>
-            <option value="floor4">Floor 4</option>
-            <option value="floor5">Floor 5</option>
-            <option value="floor6">Floor 6</option>
-            <option value="floor7">Floor 7</option>
+          <select name="elevator" onChange={({ target }) => this.handleFloorChange(target.value)}>
+            <option value="1">Floor 1</option>
+            <option value="2">Floor 2</option>
+            <option value="3">Floor 3</option>
+            <option value="4">Floor 4</option>
+            <option value="5">Floor 5</option>
+            <option value="6">Floor 6</option>
+            <option value="7">Floor 7</option>
           </select>
-          <button name="pick-up">Pick Up</button>
+          { this.state.floor.key === 'floor4' && makePizzaButton }
+          <button name="pick-up" value={ this.state.pickUpValue } onClick={({ target }) => this.handlePickUp(target.value)}>Pick Up</button>
           <button name="drop">Drop</button>
-          <button name="pizza-add">Add to Pizza</button>
+          <button name="pizza-add" value={ this.state.itemInHand } onClick={({ target }) => {this.handleAddtoPizza(target.value)}}>Add to Pizza</button>
           <label>Items in the Room</label>
-          <select name="items-in-room" onChange={({ target }) => {console.log ('select value', target.value);}}>{ this.state.floor.items.map((item) => {
+          <select name="items-in-room" onChange={({ target }) => {
+            console.log('item in hand 232323232323', this.state.itemInHand)
+            this.handlePickUpValue(target.value)}}>
+          { this.state.floor.items.map((item) => {
             return <option value={item}>{item}</option>;
-          }) }</select>
+          }) }
+          </select>
           
 
 
--- src/Floors.js
+++ src/Floors.js
@@ -11,6 +11,36 @@ const floor2 = {
   items: ['crust', 'couch']
 };
 
-const floors = [floor1, floor2];
+const floor3 = {
+  key: 'floor3',
+  message: 'You are on floor 3',
+  items: ['sauce', 'couch']
+};
+
+const floor4 = {
+  key: 'floor4',
+  message: 'You are on floor 4',
+  items: ['sauce', 'toolkit', 'parrot', 'tv']
+};
+
+const floor5 = {
+  key: 'floor5',
+  message: 'You are on floor 5',
+  items: ['sauce', 'couch']
+};
+
+const floor6 = {
+  key: 'floor6',
+  message: 'You are on floor 6',
+  items: ['sauce', 'cheese']
+};
+
+const floor7 = {
+  key: 'floor7',
+  message: 'You are on floor 7',
+  items: ['pepperoni', 'cheese']
+};
+
+const floors = [floor1, floor2, floor3, floor4, floor5, floor6, floor7];
 
 export default floors;
