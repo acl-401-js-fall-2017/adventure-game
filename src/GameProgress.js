b3b48ff9648bf96a13d0c97d668f089039e676d3
Robin Miller <rmillerpdx@gmail.com>
2017-11-13 12:57:36 -0800

    refactor gameprogress code

--- src/GameProgress.js
+++ src/GameProgress.js
@@ -3,10 +3,12 @@ import styled from 'styled-components';
  
 export default class GameProgress extends Component{
 state ={
-  outcome:''
+  outcome:'',
+  outcomeArray: []
 }
   
   resolveAction= (playersReady) =>{
+    const updatedArray = this.state.outcomeArray;
     const { setNextRound, bobAction, jeffAction, updateHealth } = this.props;
     if (!playersReady) return;
     //Player One: Quick
@@ -14,15 +16,15 @@ state ={
       if(jeffAction === 'quick') {
         updateHealth(-1, 'One');
         updateHealth(-1, 'Two');
-        this.setState({ outcome: 'Both grown men stab each other at the same time, suffering 1 dmg each.' });
+        updatedArray.push('Both grown men stab each other at the same time, suffering 1 dmg each.');
       }
       if(jeffAction === 'heavy') {
         updateHealth(-2, 'Two');
-        this.setState({ outcome: 'An enraged Jeff swung at Bob with a tire iron, only to be countered by a swift poke in the eye by Bob\'s umbrella. Jeff suffers 2 dmg.' });
+        updatedArray.push('An enraged Jeff swung at Bob with a tire iron, only to be countered by a swift poke in the eye by Bob\'s umbrella. Jeff suffers 2 dmg.');
       } 
       if(jeffAction === 'riposte') {
         updateHealth(-2, 'One');
-        this.setState({ outcome: 'Bob attempts a quick umbrella stab, only to be interrupted by a swift counter riposte from Jeff\'s tire iron. Bob suffers 2 dmg.' });
+        updatedArray.push('Bob attempts a quick umbrella stab, only to be interrupted by a swift counter riposte from Jeff\'s tire iron. Bob suffers 2 dmg.');
       } 
     }
 
@@ -30,16 +32,16 @@ state ={
     if (bobAction === 'heavy'){
       if(jeffAction === 'quick') {
         updateHealth(-2, 'One');
-        this.setState({ outcome: 'Bob attempts a heavy umbrella swing, only to get blindsided by Jeff\'s swift kick to the shin. Bob suffers 2 dmg.' });
+        updatedArray.push('Bob attempts a heavy umbrella swing, only to get blindsided by Jeff\'s swift kick to the shin. Bob suffers 2 dmg.');
       }
       if(jeffAction === 'heavy') {
         updateHealth(-2, 'Two');
         updateHealth(-2, 'One');
-        this.setState({ outcome: 'Both adult men attempt heavy attacks on one another, each connecting successfully. Both mutter \'I\'m getting too old for this shit\' under their breath and suffer 2 dmg.' });
+        updatedArray.push('Both adult men attempt heavy attacks on one another, each connecting successfully. Both mutter \'I\'m getting too old for this shit\' under their breath and suffer 2 dmg.');
       } 
       if(jeffAction === 'riposte') {
         updateHealth(-2, 'Two');
-        this.setState({ outcome: 'Jeff tries an elaborate counter attack, only to get humiliated by Bob\'s crushing overhead umbrella attack. Jeff suffers 2 dmg.' });
+        updatedArray.push('Jeff tries an elaborate counter attack, only to get humiliated by Bob\'s crushing overhead umbrella attack. Jeff suffers 2 dmg.');
       } 
     }
 
@@ -47,18 +49,19 @@ state ={
     if (bobAction === 'riposte'){
       if(jeffAction === 'quick') {
         updateHealth(-2, 'Two');
-        this.setState({ outcome: 'Bob anticipates Jeff\'s tire iron jab, countering it with a flurry of umbrella pokes to the face. Jeff suffers 2 dmg and permanent scars.' });
+        updatedArray.push('Bob anticipates Jeff\'s tire iron jab, countering it with a flurry of umbrella pokes to the face. Jeff suffers 2 dmg and permanent scars.');
       }
       if(jeffAction === 'heavy') {
         updateHealth(-2, 'One');
-        this.setState({ outcome: 'Hoping to outsmart Jeff, Bob prepares a counter-attack, only to be overpowered by Jeff\'s mighty tire iron swing. Bob suffers 2 dmg.' });
+        updatedArray.push('Hoping to outsmart Jeff, Bob prepares a counter-attack, only to be overpowered by Jeff\'s mighty tire iron swing. Bob suffers 2 dmg.');
       } 
       if(jeffAction === 'riposte') {
         updateHealth(1, 'One');
         updateHealth(1, 'Two'); 
-        this.setState({ outcome: 'Both Bob and Jeff attempt to counter attack each other\'s attacks. For a moment, they meet each other\'s gaze and contemplate what they\'re doing with their lives. Rejuvinated by their introspection, both men gain +1 health' });
+        updatedArray.push('Both Bob and Jeff attempt to counter attack each other\'s attacks. For a moment, they meet each other\'s gaze and contemplate what they\'re doing with their lives. Rejuvinated by their introspection, both men gain +1 health');
       } 
     }
+    this.setState({ outcomeArray : updatedArray });
     setNextRound();
   };
 
@@ -73,7 +76,7 @@ state ={
       <InfoDiv>
         <span>player one chose to {bobAction}</span>
         <span>player Two chose to {jeffAction}</span>
-        <span> Outcome: {this.state.outcome}</span>
+        {this.state.outcomeArray.map((outcome, i) => <li key={i}>{outcome}</li>)}
       </InfoDiv>
     );
   }
