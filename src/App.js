6adadd2a9b06e52766f2f204d3eab2e1dec82692
Andrew Bodey <a.s.bodey@gmail.com>
2017-11-13 10:28:29 -0800

    trying to add to inv

--- src/App.js
+++ src/App.js
@@ -1,51 +1,60 @@
 import React, { Component } from 'react';
 import logo from './goldenTx.jpg';
+import shop from './wonkaStore.jpg';
+
 import './App.css';
 
 class App extends Component {
+  
   constructor() {
     super();
     this.state = {
-      message: ''
+      shopMessage: 'You have got the golden ticket',
+      message: '',
+      inventory: ''
     };
   }
   
-  handleClick(className){
+  handleClick(className, id){
     this.setState({  
-      message: className !== 'correct' ? 'Pick again' : 'You have got the golden ticket' 
+      inventory: id === 'golden-ticket' ? this.state.inventory + 'golden-ticket' : null,
+      message: className !== 'correct' ? 'Pick again' : this.state.shopMessage
     });
     
   }
   
   render() {
+    const { inventory } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">The Wonka Tour</h1>
+          <h2> Your Inventory: {inventory}</h2>
         </header>
         <p className="App-pic">
-          <img src="https://i.ytimg.com/vi/sizCBeS03YU/maxresdefault.jpg" useMap="#shopMap" alt="Candy shop"/>
+          <img src={shop} useMap="#shopMap" alt="Candy shop"/>
           <map name="shopMap" id="shopMap">
-            <area alt="grid" ref="wrong" className="wrong" shape="poly" coords="91,270,95,543,394,546,381,305" onClick={event => this.handleClick(event.target.className)}/>
-            <area alt="grid" ref="wrong" className="wrong" shape="poly" coords="440,293,434,548,892,546,881,302" onClick={event => this.handleClick(event.target.className)}/>
-            <area alt="grid" ref="wrong" className="wrong" shape="poly" coords="91,556,84,707,380,706,381,566" onClick={event => this.handleClick(event.target.className)}/>
-            <area alt="grid" ref="wrong" className="wrong" shape="poly" coords="435,564,434,703,892,709,892,563" onClick={event => this.handleClick(event.target.className)}/>
-            <area alt="grid" ref="wrong" className="wrong" shape="poly" coords="88,726,90,870,375,860,376,720" onClick={event => this.handleClick(event.target.className)}/>
-            <area alt="grid" ref="wrong" className="wrong" shape="poly" coords="432,720,434,862,889,878,894,724" onClick={event => this.handleClick(event.target.className)}/>
-            <area alt="grid" ref="wrong" className="wrong" shape="poly" coords="1458,760,1462,944,1698,978,1696,775" onClick={event => this.handleClick(event.target.className)}/>
-            <area alt="grid" ref="wrong" className="wrong" shape="poly" coords="1458,546,1465,737,1696,746,1692,546" onClick={event => this.handleClick(event.target.className)}/>
-            <area alt="grid" ref="wrong" className="wrong" shape="poly" coords="1458,311,1462,530,1697,519,1694,310" onClick={event => this.handleClick(event.target.className)}/>
-            <area alt="grid" ref="wrong" className="wrong" shape="poly" coords="1729,777,1736,989,1918,1013,1917,786" onClick={event => this.handleClick(event.target.className)}/>
-            <area alt="grid" ref="wrong" className="wrong" shape="poly" coords="1734,522,1744,319,1914,305,1918,516" onClick={event => this.handleClick(event.target.className)}/>
-            <area alt="grid" ref="correct" className="correct" shape="poly" coords="1734,542,1741,748,1919,759,1914,535" onClick={event => this.handleClick(event.target.className)} />
+            <area alt="grid"  className="wrong" shape="poly" coords="53,194,52,320,220,324,220,208" onClick={event => this.handleClick(event.target.className)}/>
+            <area alt="grid"  className="wrong" shape="poly" coords="61,342,57,415,216,416,221,345" onClick={event => this.handleClick(event.target.className)}/>
+            <area alt="grid"  className="wrong" shape="poly" coords="56,444,52,518,205,516,220,443" onClick={event => this.handleClick(event.target.className)}/>
+            <area alt="grid"  className="wrong" shape="poly" coords="262,208,261,326,531,327,524,208" onClick={event => this.handleClick(event.target.className)}/>
+            <area alt="grid"  className="wrong" shape="poly" coords="262,353,261,417,530,422,528,346" onClick={event => this.handleClick(event.target.className)}/>
+            <area alt="grid"  className="wrong" shape="poly" coords="265,440,261,507,523,511,536,438" onClick={event => this.handleClick(event.target.className)}/>
+            <area alt="grid"  className="wrong" shape="poly" coords="881,202,881,312,1016,305,1008,201" onClick={event => this.handleClick(event.target.className)}/>
+            <area alt="grid"  className="wrong" shape="poly" coords="880,337,881,434,1014,444,1016,332" onClick={event => this.handleClick(event.target.className)}/>
+            <area alt="grid"  className="wrong" shape="poly" coords="1055,198,1047,306,1144,305,1144,200" onClick={event => this.handleClick(event.target.className)}/>
+            <area alt="grid"  className="wrong" shape="poly" coords="884,461,883,558,1013,581,1012,468" onClick={event => this.handleClick(event.target.className)}/>
+            <area alt="grid"  className="wrong" shape="poly" coords="1045,467,1045,583,1148,603,1145,474" onClick={event => this.handleClick(event.target.className)}/>
+            <area alt="grid"  className="wrong" shape="poly" coords="872,591,871,643,988,641,985,590" onClick={event => this.handleClick(event.target.className)}/>
+            <area alt="grid"  id="golden-ticket" className="correct" shape="poly" coords="1049,337,1049,447,1150,452,1146,335" onClick={event => this.handleClick(event.target.className)}/>
           </map>
         </p>
         <p className="App-script"> 
         Pick what candy to buy! Try to get the 'Golden Ticket'. 
         </p>
         <p> 
-          Message: {this.state.message} 
+          Message: {this.state.message}
         </p>
       </div>
     );
@@ -54,3 +63,4 @@ class App extends Component {
 
 
 export default App;
+// "https://imgur.com/a/in2di"
