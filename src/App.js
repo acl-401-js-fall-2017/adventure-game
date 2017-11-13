9a8609ec00610a9132513dc8a9bad23b21cf823a
ckrountree <jmail0207@gmail.com>
2017-11-13 11:05:23 -0800

    add state, create methods populating grid

--- src/App.js
+++ src/App.js
@@ -1,18 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const terrain = {
+  forest: {
+    type: 'forest',
+    color: 'green',
+    fightProbability: 0.2
+  },
+  field: {
+    type: 'field',
+    color: 'lightgreen',
+    fightProbability: 0.02
+    
+  }
+};
+  
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      gridArray:[],
+      gnomeStats: {
+        health: 100,
+        strength: 1,
+        pos: {
+          X: 9,
+          Y: 9
+        }
+      }
+    };
+
+    this.settings = {
+      grid: {
+        height: 10,
+        width: 10
+      }
+    };
+  }
+
+  setGnomeOnGrid(gridArray){
+    const { gnomeStats } = this.state;
+    gridArray[gnomeStats.pos.Y][gnomeStats.pos.X].hasGnome = true;
+  }
+
+  componentWillMount() {
+    const gridSetUp = [];
+    for(let i = 0; i < 10; i++) {
+      gridSetUp[i] = [];
+      for(let k = 0; k < 10; k++) {
+        gridSetUp[i][k] = {
+          terrain: i < 3 ? terrain.forest : terrain.field,
+          hasGnome: false
+        };
+      }
+    }
+
+    this.setGnomeOnGrid(gridSetUp);
+    console.log(gridSetUp);
+    this.setState({ gridArray: gridSetUp });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
