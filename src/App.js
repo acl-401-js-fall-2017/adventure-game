d100a7433aeee50fb7445e61114342d7abf4c8ef
Shane Moyo <smoyo@pdx.edu>
2017-11-13 21:05:27 -0800

    refactoring components

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ class App extends Component {
   }
 
   componentWillMount() {
-    const savedGame = localStorage.getItem('currentGame')
+    const savedGame = localStorage.getItem('currentGame');
     savedGame ? this.setState(JSON.parse(savedGame)) :
-    this.setState({ timer: { minutes: parseInt(moment().format('mm'), 10), seconds: parseInt(moment().format('ss'), 10)} });
+      this.setState({ timer: { minutes: parseInt(moment().format('mm'), 10), seconds: parseInt(moment().format('ss'), 10)} });
   }
 
   componentDidUpdate() {
@@ -66,24 +66,15 @@ class App extends Component {
   render() {
 
     const makePizzaButton = <button name="make-pizza" onClick={() => this.handleMakePizza()}>make pizza</button>;
-    const { floor } =this.state;
+    const { floor, floors } =this.state;
     return (
       <div className="App">
-        <Floor floorScript ={ floor.message }/>
+        <Floor floorScript ={floor.message}/>
         <div className="wrapper">
-          <select className="dropDown one" name="elevator" onChange={({ target }) => this.handleFloorChange(target.value)}>
-            <option value="0">Ground floor</option>
-            <option value="1">Floor 1</option>
-            <option value="2">Floor 2</option>
-            <option value="3" defaultValue>Home</option>
-            <option value="4">Floor 4</option>
-            <option value="5">Floor 5</option>
-            <option value="6">Floor 6</option>
-          </select>
-          { this.state.floor.key === 'floor3' && makePizzaButton }
-          <button className="pick-up two" value={this.state.pickUpValue} onClick={({ target }) => this.handlePickUp(target.value)}>Pick Up</button>
-          <button className="drop three" onClick= {() => this.handleDrop()}>Drop</button>
-          <button className="pizza-add four" value={this.state.itemInHand} onClick={({ target }) => {this.handleAddtoPizza(target.value);}}>Add to Pizza</button>
+          <Elevator floors={floors}floorChange ={floorNumber => this.handleFloorChange(floorNumber)}/>
+          { floor.key === '4th Floor' && makePizzaButton }
+          <Controller pickUpVal={this.state.pickUpValue} pickUp={item => this.handlePickUp(item)} 
+            addToPizza={item => this.handleAddtoPizza(item)} drop={()=> this.handleDrop()}/>
           <img id="pics"alt="Old Lady" src={this.state.floor.img}/>
           <label>Items in the Room</label>
           <select className="dropDown five" name="items-in-room" onChange={({ target }) => {
@@ -103,11 +94,41 @@ class Floor extends Component {
   render(){
     const { floorScript } = this.props;
     return (
-  <header className="App-header">
-    <h1 className="flash">Shane is lounging at home</h1>
-    <p className="msg">{ floorScript }</p>
-  </header>)
+      <header className="App-header">
+        <h1 className="flash">Shane is lounging at home</h1>
+        <p className="msg">{floorScript}</p>
+      </header>
+    );
   }
 }
 
+class Elevator extends Component {
+  render(){
+    const { floorChange, floors } = this.props;
+    const elevatorOptions = floors.map((floor, index) => {
+      const option = floor.key === '4th Floor' ? 
+        <option key={index} value={index} default>Home</option> :
+        <option key ={index} value={index}>{floor.key}</option>;
+      return option;
+    });
+    return(
+      <select className="dropDown one" name="elevator" onChange={({ target }) => floorChange(target.value)}>
+        { elevatorOptions }
+      </select>
+    );
+  }
+}
+
+class Controller extends Component {
+  render(){
+    const { addToPizza, pickUp, pickUpVal, drop, holding } =this.props;
+    return(
+      <div>
+        <button className="pick-up two" value={pickUpVal} onClick={({ target }) => pickUp(target.value)}>Pick Up</button>
+        <button className="drop three" onClick= {() => drop()}>Drop</button>
+        <button className="pizza-add four" value={holding} onClick={({ target }) => addToPizza(target.value)}>Add to Pizza</button>
+      </div>
+    );
+  }
+}
 export default App;
--- src/Floors.js
+++ src/Floors.js
@@ -1,44 +1,44 @@
 
 
 const floor0 = {
-  key: 'floor0',
+  key: 'Ground Floor',
   message: 'You are on the Ground Floor and meet up with Helga, the apartment chatterer. You try to explain your quest for pizza ingredients but you cannot get a word in. After hearing all about her ailing body parts, you realize that you have been standing there so long, you are dizzy from hunger. You lose time on your quest.',
   items: ['pepperoni', 'cheese'],
   img: '/images/oldLady.jpg'
 };
 
 const floor1 = {
-  key: 'floor1',
+  key: '1st Floor',
   message: 'You are on floor 1',
   items: ['cheese', 'candy']
 };
 
 const floor2 = {
-  key: 'floor2',
+  key: '2nd Floor',
   message: 'You are on floor 2',
   items: ['crust', 'couch']
 };
 
 const floor3 = {
-  key: 'floor3',
+  key: '3rd Floor',
   message: 'You are at home',
   items: []
 };
 
 const floor4 = {
-  key: 'floor4',
+  key: '4th Floor',
   message: 'You are on floor 4',
   items: ['sauce', 'toolkit', 'parrot', 'tv']
 };
 
 const floor5 = {
-  key: 'floor5',
+  key: '5th Floor',
   message: 'You are on floor 5',
   items: ['sauce', 'couch']
 };
 
 const floor6 = {
-  key: 'floor6',
+  key: '6th Floor',
   message: 'You are on floor 6',
   items: ['sauce', 'cheese']
 };
