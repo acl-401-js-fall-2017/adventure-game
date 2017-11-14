1931b134f8d54c6121a4363893e875ac40182e79
mirrorapps <eli.speigel@gmail.com>
2017-11-14 13:06:30 -0800

    built out info

--- src/stages.js
+++ src/stages.js
@@ -1,13 +1,20 @@
 const freezer = {
   name: 'Walk-in Freezer',
-  intro: 'Suddenly conscious and taking in your surroundings, a frosty haze permeates your view of a large, dim walk-in freezer. You notice a bright light at the far side of the freezer, but first a sign catches your eye.',
+  door: {
+    name: 'Air-conditioned cafe',
+    available: false
+  },
+  intro: {
+    text: 'Suddenly conscious and taking in your surroundings, a frosty haze permeates your view of a large, dim walk-in freezer. You notice a bright light at the far side of the freezer, but first a sign catches your eye.',
+    read: false
+  },
   items: [
     {
-      name: 'joints',
+      name: 'key',
       available: false
     },
     {
-      name: 'cafeKey',
+      name: 'joints',
       available: false
     }
   ],
@@ -21,53 +28,52 @@ const freezer = {
     ],
     solved: false
   },
-  door: 'Air-conditioned cafe',
   encounter: {
     name: 'Jenkins',
-    dialogue: [
-      [
+    dialog: {
+      playerDialog: [
         'Welcome to the Walk-in Freezer. My name is Jenkins, and I’m the caretaker here. Who are you?',
         'We are in a freezer, but I suppose it’s a good thing you have your parka, for now…',
         'To escape this nightmare you must descend through all four stages of heat, solving a puzzle on each floor, until you reach the blazing depths of hell and defeat the devil.  I would wish you luck,but as you said, ‘man’s not hot’.',
       ],
-      [
+      encounterDialog: [
         'Skrrat, skidi-kat-kat. The name’s Big Shaq and man’s not hot.',
         'Hold up let me light this tree right quick. puff puff puff. How do I get out of here anyway?',
         'Whatever donut, your nose  long like a garden hose anyway.'
       ]
-    ]
+    }
   }
 };
 
 const cafe = {
-  key: 'cafe',
   name: 'Air-conditioned cafe',
-  sign: '',
+  door: 'Picnic in the Park',
+  key: 'cafe',
+  intro: '',
   items: [],
-  door: 'Picnic in the Park'
 };
 
 const picnic = {
-  key: 'picnic',
   name: 'Picnic in the Park',
-  sign: '',
+  door: 'Ghana',
+  key: 'picnic',
+  intro: '',
   items: [],
-  door: 'Ghana'
 };
 
 const ghana = {
-  key: 'ghana',
   name: 'Ghana',
-  sign: '',
+  door: 'Hell',
+  key: 'ghana',
+  intro: '',
   items: [],
-  door: 'Hell'
 };
 
 const hell = {
-  key: 'hell',
   name: 'Hell',
-  sign: '',
+  key: 'hell',
   items: [],
+  intro: '',
   encounter: {}
 };
 
