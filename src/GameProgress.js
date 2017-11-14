fec308ab619fb7d4014b8651b9a75e5355929c2b
Robin Miller <rmillerpdx@gmail.com>
2017-11-14 12:24:24 -0800

    fix log text

--- src/GameProgress.js
+++ src/GameProgress.js
@@ -17,26 +17,19 @@ scrollToBottom = () => {
     const { setNextRound, playerOne, playerTwo, updateHealth, astralProject } = this.props;
     if (!playersReady) return;
 
-    //Player One: Quick
-    // take the red pill:
-    // const result = resolutions[playerOne.action][playerTwo.action];
-    //   updateHealth(result.damageOne, 'One');
-    //   updateHealth(result.damageTwo, 'two');
-    //   updatedArray.push(result.text);
-
     if (playerOne.action === 'dog'){
       if(playerTwo.action === 'quick') {
         updateHealth(-1, 'One');
         updateHealth(-3, 'Two');
-        updatedArray.push(`${playerTwo.name} tried to stab ${playerOne.name} with a pointy end of his tire iron, only to be blindsided by ${playerOne.dog}\'s ferocious bite to his ankle. ${playerTwo.name} manages to swap the dog away causing 1 emotional dmg to ${playerOne.name}. ${playerTwo.name} ankle suffers 2 dmg.`);
+        updatedArray.push(`${playerTwo.name} tried to stab ${playerOne.name} with a pointy end of his tennis racket, only to be blindsided by ${playerOne.dog}'s ferocious bite to his ankle. ${playerTwo.name} managed to swat the dog away causing 1 emotional dmg to ${playerOne.name}. ${playerTwo.name}'s ankle suffers 2 dmg.`);
       }
       if(playerTwo.action === 'heavy') {
         updateHealth(-3, 'Two');
-        updatedArray.push(`relying on his might ${playerTwo.name} swung at ${playerOne.name} with a rolled up forbes newspaper. Sensing the opening, ${playerOne.dog} bit ${playerTwo.name}\'s butt. struggling to get ${playerOne.dog} off his back, ${playerTwo.name} suffers devastating 3 dmg.`);
+        updatedArray.push(`${playerTwo.name} swung at ${playerOne.name} with his tennis racket. Sensing the opening, ${playerOne.dog} bit ${playerTwo.name}'s butt. Struggling to get ${playerOne.dog} off his back, ${playerTwo.name} suffers a devastating 3 dmg.`);
       } 
       if(playerTwo.action === 'riposte') {
         updateHealth(-2, 'One');
-        updatedArray.push(`Unaware of ${playerTwo.name}\'s trap, ${playerOne.name} directed ${playerOne.dog} to attack only to be countered by ${playerTwo.name}\'s riposte. Seeing his pet beaten so brutally causes ${playerOne.name} 3 psychological  damage and thousands in future therapy bills.`);
+        updatedArray.push(`Unaware of ${playerTwo.name}\'s trap, ${playerOne.name} directed ${playerOne.dog} to attack, only to be countered by ${playerTwo.name}'s riposte. Seeing his pet beaten so brutally causes ${playerOne.name} 3 psychological dmg and thousands in future therapy bills.`);
       } 
     }
     if (playerOne.action === 'quick'){
@@ -47,15 +40,15 @@ scrollToBottom = () => {
       }
       if(playerTwo.action === 'heavy') {
         updateHealth(-2, 'Two');
-        updatedArray.push(`An enraged ${playerTwo.name} swung at ${playerOne.name} with a tire iron, only to be countered by a swift poke in the eye by ${playerOne.name}\'s umbrella. ${playerTwo.name} suffers 2 dmg.`);
+        updatedArray.push(`An enraged ${playerTwo.name} swung at ${playerOne.name} with a tennis racket, only to be countered by a swift poke in the eye by ${playerOne.name}\'s umbrella. ${playerTwo.name} suffers 2 dmg.`);
       } 
       if(playerTwo.action === 'riposte') {
         updateHealth(-2, 'One');
-        updatedArray.push(`${playerOne.name} attempts a quick umbrella stab, only to be interrupted by a swift counter riposte from ${playerTwo.name}\'s tire iron. ${playerOne.name} suffers 2 dmg.`);
+        updatedArray.push(`${playerOne.name} attempts a quick umbrella stab, only to be interrupted by a swift counter riposte from ${playerTwo.name}'s tennis racket. ${playerOne.name} suffers 2 dmg.`);
       }
       if(playerTwo.action === 'astralProject') {
         astralProject();
-        updatedArray.push(`${playerTwo.name}'s spirit leaves his body and possesses ${playerOne.name}. Players switch hp totals. `);
+        updatedArray.push(`${playerTwo.name}'s spirit leaves his body and possesses ${playerOne.name}. Players switch hp totals.`);
       }  
     }
 
@@ -63,16 +56,16 @@ scrollToBottom = () => {
     if (playerOne.action === 'heavy'){
       if(playerTwo.action === 'quick') {
         updateHealth(-2, 'One');
-        updatedArray.push(`${playerOne.name} attempts a heavy umbrella swing, only to get blindsided by ${playerTwo.name}\'s swift kick to the shin. ${playerOne.name} suffers 2 dmg.`);
+        updatedArray.push(`${playerOne.name} attempts a heavy umbrella swing, only to get blindsided by ${playerTwo.name}'s swift kick to the shin. ${playerOne.name} suffers 2 dmg.`);
       }
       if(playerTwo.action === 'heavy') {
         updateHealth(-2, 'Two');
         updateHealth(-2, 'One');
-        updatedArray.push('Both adult men attempt heavy attacks on one another, each connecting successfully. Both mutter \'I\'m getting too old for this shit\' under their breath and suffer 2 dmg.');
+        updatedArray.push('Both adult men attempt heavy attacks on one another, each connecting successfully. Both mutter "I\'m getting too old for this shit" under their breath and suffer 2 dmg.');
       } 
       if(playerTwo.action === 'riposte') {
         updateHealth(-2, 'Two');
-        updatedArray.push(`${playerTwo.name} tries an elaborate counter attack, only to get humiliated by ${playerOne.name}\'s crushing overhead umbrella attack. ${playerTwo.name} suffers 2 dmg.`);
+        updatedArray.push(`${playerTwo.name} tries an elaborate counter attack, only to get humiliated by ${playerOne.name}'s crushing overhead umbrella attack. ${playerTwo.name} suffers 2 dmg.`);
       } 
       if(playerTwo.action === 'astralProject') {
         astralProject();
@@ -84,16 +77,16 @@ scrollToBottom = () => {
     if (playerOne.action === 'riposte'){
       if(playerTwo.action === 'quick') {
         updateHealth(-2, 'Two');
-        updatedArray.push(`${playerOne.name} anticipates ${playerTwo.name}\'s tire iron jab, countering it with a flurry of umbrella pokes to the face. ${playerTwo.name} suffers 2 dmg and permanent scars.`);
+        updatedArray.push(`${playerOne.name} anticipates ${playerTwo.name}'s tennis racket jab, countering it with a flurry of umbrella pokes to the face. ${playerTwo.name} suffers 2 dmg and permanent scars.`);
       }
       if(playerTwo.action === 'heavy') {
         updateHealth(-2, 'One');
-        updatedArray.push(`Hoping to outsmart ${playerTwo.name}, ${playerOne.name} prepares a counter-attack, only to be overpowered by ${playerTwo.name}\'s mighty tire iron swing. ${playerOne.name} suffers 2 dmg.`);
+        updatedArray.push(`Hoping to outsmart ${playerTwo.name}, ${playerOne.name} prepares a counter-attack, only to be overpowered by ${playerTwo.name}'s mighty tennis racket swing. ${playerOne.name} suffers 2 dmg.`);
       } 
       if(playerTwo.action === 'riposte') {
         updateHealth(1, 'One');
         updateHealth(1, 'Two'); 
-        updatedArray.push(`Both ${playerOne.name} and ${playerTwo.name} attempt to counter attack each other\'s attacks. For a moment, they meet each other\'s gaze and contemplate what they\'re doing with their lives. Rejuvinated by their introspection, both men gain +1 health`);
+        updatedArray.push(`Both ${playerOne.name} and ${playerTwo.name} attempt to counter attack each other's attacks. For a moment, they meet each other's gaze and contemplate what they're doing with their lives. Rejuvinated by their introspection, both men gain +1 health`);
       }
       if(playerTwo.action === 'astralProject') {
         const jeffHp = - (playerTwo.hp - 1);
