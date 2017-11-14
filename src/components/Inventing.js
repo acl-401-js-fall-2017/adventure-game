e39442be806c73a9db4e85874c124c98b3197567
Zac-L <Zachlichens@gmail.com>
2017-11-14 11:42:03 -0800

    added array for kids, refactored if/else for the kids and gobstopper

--- src/components/Inventing.js
+++ src/components/Inventing.js
@@ -7,56 +7,54 @@ export default class Inventing extends Component {
   constructor() {
     super();
     this.state = {
-      invMessage: 'You got the Gobstopper',
-      people: []
+      kids: [],
+      gotGob: false
     };
   }
 
   handleClick(value, inv){
     const { handleInventory, inventory } = this.props;
+    const kids = [...this.state.kids];
+    if(this.state.kids.length <= 3 && inv === 'kids') {
+      kids.push('kids');
+    }
+    const gotGob = inventory.indexOf('gobstopper') === -1 && inv === 'gobstopper' && kids.length >= 4 ? true : false;
     this.setState({
-      message: value !== 'correct' ? 'There are still children in your way' : this.state.invMessage
+      kids,
+      gotGob
     },() =>  {
-      if (inventory.indexOf('gobstopper') === -1 && inv === 'gobstopper') {
+      if (inventory.indexOf('gobstopper') === -1 && inv === 'gobstopper' && this.state.kids.length >= 4) {
         handleInventory('gobstopper');
       }
     });
   }
 
-  // handleShove(value, people) {
-  //   const { removeInventory, people } = this.props;
-  //   this.setState({
-  //     message: people.indexOf('golden-ticket') === -1 ? 'You need a ticket to get in' : this.state.invMessage
-  //   }, () => {
-  //     if (people.indexOf('golden-ticket') > -1) {
-  //       removeInventory();
-  //     }
-  //   });
-  // }
-
 
   render() {
+    let message = 'Grab the gobstopper';
+    if(this.state.length < 4) {
+      message = 'There are still children in your way';
+    } else if(this.state.gotGob === true) {
+      message = 'You got the gobstopper!';
+    }
     
     return (
       <section>
         <img src={inventPic} alt="" useMap="#invMap" />
         <map name="invMap" id="Map">
-          <area alt="grid" data-value="correct" data-inv="gobstopper" href="#" shape="poly" coords="436,249,436,295,486,295,486,242" 
-            onClick={({ target }) => this.handleClick(target.dataset.value)}/>
-          <area alt="grid" data-value="person" href="#" shape="poly" coords="6,432,8,635,163,631,222,442" 
-            onClick={({ target }) => this.handleClick(target.dataset.value)}/>
-          <area alt="grid" data-value="person" href="#" shape="poly" coords="246,324,207,613,458,618,383,321" 
-            onClick={({ target }) => this.handleClick(target.dataset.value)}/>
-          <area alt="grid" data-value="person" href="#" shape="poly" coords="467,609,580,304,743,324,784,608" 
-            onClick={({ target }) => this.handleClick(target.dataset.value)}/>
-          <area alt="grid" data-value="person" href="#" shape="poly" coords="795,602,918,295,1085,292,1142,590"   
-            onClick={({ target }) => this.handleClick(target.dataset.value)}/>
+          <area alt="grid" data-value="correct" data-inv="gobstopper" shape="poly" coords="436,249,436,295,486,295,486,242" onClick={({ target }) => this.handleClick(target.dataset.value, target.dataset.inv)}/>
+          <area alt="grid" data-value="person" data-inv="kids" shape="poly" coords="6,432,8,635,163,631,222,442" onClick={({ target }) => this.handleClick(target.dataset.value, target.dataset.inv)}/>
+          <area alt="grid" data-value="person" data-inv="kids" shape="poly" coords="246,324,207,613,458,618,383,321" onClick={({ target }) => this.handleClick(target.dataset.value, target.dataset.inv)}/>
+          <area alt="grid" data-value="person" data-inv="kids" shape="poly" coords="467,609,580,304,743,324,784,608" onClick={({ target }) => this.handleClick(target.dataset.value, target.dataset.inv)}/>
+          <area alt="grid" data-value="person" data-inv="kids" shape="poly" coords="795,602,918,295,1085,292,1142,590"   onClick={({ target }) => this.handleClick(target.dataset.value, target.dataset.inv)}/>
         </map>
-        <p className="App-script">
-          Move the 4 kids out of the way to get the everlasting gobstopper.
-        </p>
+        {this.state.kids.length < 4 && (
+          <p className="App-script"> 
+          Move the { 4 - this.state.kids.length} kids out of the way to get the everlasting gobstopper.
+          </p>
+        )}
         <p>
-          Message: {this.state.message}
+          Message: {message} 
         </p>
       </section>
     );
