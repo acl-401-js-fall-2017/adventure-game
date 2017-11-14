9ca0dccffad9a66ba2a5e0aa13e8019ee5fd5a00
Andrew Bodey <a.s.bodey@gmail.com>
2017-11-14 12:20:15 -0800

    staff done

--- src/components/Staff.js
+++ src/components/Staff.js
@@ -3,20 +3,20 @@ import staffPic from '../pics/staff.jpg';
 
 
 
-export default class Gate extends Component {
+export default class Staff extends Component {
   constructor() {
     super();
     this.state = {
-      gateMessage: 'Welcome to the Chocolate factory',
+      staffMessage: 'You won the Chocolate factory',
     };
   }
 
   handleClick(value, inv){
     const { removeInventory, inventory } = this.props;
     this.setState({
-      message: inventory.indexOf('golden-ticket') === -1 ? 'You need a ticket to get in' : this.state.gateMessage
+      message: inventory.indexOf('gobstopper') === -1 ? 'You need to give Mr Wonka a Gobstopper' : this.state.staffMessage
     },() =>  {
-      if(inventory.indexOf('golden-ticket') > -1) {
+      if (inventory.indexOf('gobstopper') > -1) {
         removeInventory();
       }
     });
@@ -28,8 +28,7 @@ export default class Gate extends Component {
       <section>
         <img src={staffPic} alt="" useMap="#staffMap" />
         <map name="staffMap" id="Map">
-          <area data-value="correct" alt="grid" shape="poly" coords="326,542,423,500,543,539,450,582" 
-            onClick={({ target }) => this.handleClick(target.dataset.value)} />
+          <area data-inv="gobstopper" alt="grid" shape="poly" coords="326,542,423,500,543,539,450,582" onClick={({ target }) => this.handleClick(target.dataset.value)} />
         </map>
         <p className="App-script">
           Place the Everlasting Gobstopper on Mr Wonka's desk.
