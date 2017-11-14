bc7e835ab33f140fb485cf4b055f0df41d464725
mirrorapps <eli.speigel@gmail.com>
2017-11-14 00:01:54 -0800

    slight editing

--- src/stages.js
+++ src/stages.js
@@ -1,7 +1,10 @@
 const freezer = {
-  key: 'freezer',
   name: 'Walk-in Freezer',
   intro: 'Suddenly conscious and taking in your surroundings, a frosty haze permeates your view of a large, dim walk-in freezer. You notice a bright light at the far side of the freezer, but first a sign catches your eye.',
+  items: [
+    'joints',
+    'cafeKey'
+  ],
   riddle: {
     name: 'sign',
     question: 'You’re running a race and pass the person in 2nd place. What place are you in now?',
@@ -10,18 +13,22 @@ const freezer = {
       '2nd',
       'second'
     ],
-    items: [
-      'joints',
-      'cafeKey'
-    ]
+    solved: false
   },
   door: 'Air-conditioned cafe',
   encounter: {
     name: 'Jenkins',
     dialogue: [
-      'Welcome to the Walk-in Freezer. My name is Jenkins, and I’m the caretaker here. Who are you?',
-      'We are in a freezer, but I suppose it’s a good thing you have your parka, for now…',
-      'To escape this nightmare you must descend through all four stages of heat, solving a puzzle on each floor, until you reach the blazing depths of hell and defeat the devil.  I would wish you luck,but as you said, ‘man’s not hot’.',
+      [
+        'Welcome to the Walk-in Freezer. My name is Jenkins, and I’m the caretaker here. Who are you?',
+        'We are in a freezer, but I suppose it’s a good thing you have your parka, for now…',
+        'To escape this nightmare you must descend through all four stages of heat, solving a puzzle on each floor, until you reach the blazing depths of hell and defeat the devil.  I would wish you luck,but as you said, ‘man’s not hot’.',
+      ],
+      [
+        'Skrrat, skidi-kat-kat. The name’s Big Shaq and man’s not hot.',
+        'Hold up let me light this tree right quick. puff puff puff. How do I get out of here anyway?',
+        'Whatever donut, your nose  long like a garden hose anyway.'
+      ]
     ]
   }
 };
