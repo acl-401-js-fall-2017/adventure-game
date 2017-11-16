6cfb01ee0549c6f2b23c2dae702e2fa7bb2a9768
Shane Moyo <smoyo@pdx.edu>
2017-11-16 07:10:06 -0800

    snapshot tests added

---
+++ src/__tests__/GameComponents.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import GameInfo from '../GameInfo';
+import SelectBox from '../SelectBox';
+import floors from '../Floors';
+import Buttons from '../Buttons';
+import Controller from '../Buttons';
+import Floor from '../Floor';
+
+
+describe('App', () => {
+
+  it('should render game info component', () => {
+    const wrapper = shallow(<GameInfo holding="sauce" pizzaIngredients={['cheese', 'crust']} />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  it('should render SelectBox component', () => {
+    const wrapper = shallow(<SelectBox options={floors} className="dropDown one" name="elevator"
+      selectChange={floorNumber => floorNumber} />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  it('should render Buttons component', () => {
+    const wrapper = shallow(<Buttons pickUpVal="cheese" pizzaIngredients={['sauce', 'crust']} floor={floors[0]}/>);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  it('should render Controller component', () => {
+    const wrapper = shallow(<Controller floor={floors[0]} floors={floors}  pizzaIngredients={['sauce', 'crust']}
+      pickUpVal="sauce" holding="crust"/>);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  it('should render Floor component', () => {
+    const wrapper = shallow(<Floor floorScript ={floors[1].message} holding="sauce" pizzaIngredients={['sauce', 'crust']}/>);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+
+});
